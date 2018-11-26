e9c021a7268d79e2c455e1345ef22f30647f5bd1
gmzorz <gijs.oosterling@hotmail.nl>
2018-11-26 17:25:48 +0100

    Update config.cmd

--- config.cmd
+++ config.cmd
@@ -4,24 +4,18 @@ goto c
 :config
 set update=1
 	:: Automatically check for updates (recommended)
-
 set hostName=
 	:: Set player name (can be left empty, you will be prompted instead)
-	
 set map=
 	:: Set default map to load (can be left empty)
-
 set maxClients=3
 	:: Maximum clients
-
 set client_1=Luckyy
 set client_2=Delayz
 set client_3=Jebasu
 	:: extra clients can be added (client_4, client_5, etc..)
-
 set fullScreen=0
 	:: base game only (clients are windowed)
-
 set gameRes=6
 set clientRes=0
 	:: window resolution
@@ -31,9 +25,9 @@ set clientRes=0
 	:: 15 =  1600x900
 	:: 18 =  1920x1080
 	:: for more info, run cod4 and write "r_mode " in console
-
 set customMod=
 	:: Load a custom mod instead of ZorWarfare, leave empty if you wish to use the default mod
+	
 	
 	:: End of config, please do not modify anything below this point
 
